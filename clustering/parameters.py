2fd61f6a3c17929d6f3441daf044429725f47b3b
Jafree <fei.jiang1989@gmail.com>
2017-03-05 19:22:35 -0600

    Datasets place organized

--- clustering/parameters.py
+++ clustering/parameters.py
@@ -1,6 +1,6 @@
-inputfile = "datasets/karateTest.txt"
-outfile = "karateOutput_community.txt"
-embeded = ("datasets/karate_sparse_nb.txt","datasets/karate_sparse_ua.txt","datasets/karate_sparse_wa.txt","datasets/karate_nb.txt")
+inputfile = "../datasets/karateTest.txt"
+outfile = "../datasets/clustering_results/karateOutput_community.txt"
+embeded = ("../datasets/karate_sparse_nb.txt","../datasets/karate_sparse_ua.txt","../datasets/karate_sparse_wa.txt","../datasets/karate_nb.txt")
 #,"datasets/deepwalkkarateem_sorted.txt","datasets/line_vec_sorted.txt")
 dimensions = [1,2,3,4,5,10,20,30,50,100]
 cluster_number = 3 
