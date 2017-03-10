6ae1d4f9f4805158e28d946c1c9e2ae33b244bc8
Fei Jiang <fei.jiang1989@gmail.com>
2017-03-10 12:22:57 -0600

    dblp,amazon embedding results added

--- clustering/myclustering.py
+++ clustering/myclustering.py
@@ -44,20 +44,20 @@ if __name__ == '__main__':
             embeded_data = embeded_all_dimensions[:,0:max_column]
             
             # Run kmeans
-            kmeans = KMeans(n_clusters=parameters.cluster_number).fit(embeded_data)
+            #kmeans = KMeans(n_clusters=parameters.cluster_number).fit(embeded_data)
             #print kmeans.labels_
-            k_result = compute_modu_perm(graph_structure,kmeans.labels_)
-            df=df.append(pd.Series([embeded_each_file,max_column,'kmeans',k_result[0],k_result[1]],index=data_columns),ignore_index=True)
-            output_content = output_content+ "kmeans"+str(k_result)+ '\n'
-            print k_result
+            #k_result = compute_modu_perm(graph_structure,kmeans.labels_)
+            #df=df.append(pd.Series([embeded_each_file,max_column,'kmeans',k_result[0],k_result[1]],index=data_columns),ignore_index=True)
+            #output_content = output_content+ "kmeans"+str(k_result)+ '\n'
+            #print 'kmeans',k_result
 
             #Run DBSCAN   Have to tune eps
-            dbscan = DBSCAN(eps = 0.1).fit(embeded_data)
+            #dbscan = DBSCAN(eps = 0.1).fit(embeded_data)
             #print dbscan.labels_
-            DB_result= compute_modu_perm(graph_structure,dbscan.labels_)
-            df=df.append(pd.Series([embeded_each_file,max_column,'dbscan',DB_result[0],DB_result[1]],index=data_columns),ignore_index=True)
-            output_content = output_content+ 'dbscan:'+str(DB_result)+'\n'
-            print DB_result
+            #DB_result= compute_modu_perm(graph_structure,dbscan.labels_)
+            #df=df.append(pd.Series([embeded_each_file,max_column,'dbscan',DB_result[0],DB_result[1]],index=data_columns),ignore_index=True)
+            #output_content = output_content+ 'dbscan:'+str(DB_result)+'\n'
+            #print 'dbscan', DB_result
 
             #Run Agglomerative Clustering
             agglomerative = AgglomerativeClustering(n_clusters=parameters.cluster_number).fit(embeded_data)
@@ -65,7 +65,7 @@ if __name__ == '__main__':
             ag_result = compute_modu_perm(graph_structure,agglomerative.labels_)
             output_content = output_content + 'agglomerative:' + str(ag_result) + '\n'
             df=df.append(pd.Series([embeded_each_file,max_column,'agglomerative',ag_result[0],ag_result[1]],index=data_columns),ignore_index=True)
-            print ag_result
+            print 'ag', ag_result
             #Run Spectral Clustering
             spectralclu= SpectralClustering(n_clusters=parameters.cluster_number).fit(embeded_data)
             #print spectralclu.labels_
--- clustering/parameters.py
+++ clustering/parameters.py
@@ -1,7 +1,6 @@
-inputfile = "../datasets/karateTest.txt"
-outfile = "../datasets/clustering_results/karateOutput_community.csv"
-embeded = ("../datasets/karate_sparse_nb.txt","../datasets/karate_sparse_ua.txt","../datasets/karate_sparse_wa.txt","../datasets/karate_nb.txt")
-#,"datasets/deepwalkkarateem_sorted.txt","datasets/line_vec_sorted.txt")
+inputfile = "../datasets/IDBLP_continuous.txt"
+outfile = "../datasets/clustering_results/IDBLP_community.csv"
+embeded = ("../datasets/embedding_results/dblp_sparse_nb.txt","../datasets/karate_nb.txt")
 dimensions = [1,2,3,4,5,10,20,30,50,100]
-cluster_number = 3 
+cluster_number = 21000 
 
